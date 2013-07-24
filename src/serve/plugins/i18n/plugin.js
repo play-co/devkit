acf22ba7156769df8e598cba5fb09930985e0206
Mario Balibrera <mario@gameclosure.com>
2013-07-24 13:36:53 -0700

    license

--- src/serve/plugins/i18n/plugin.js
+++ src/serve/plugins/i18n/plugin.js
@@ -1,3 +1,20 @@
+/** @license
+ * This file is part of the Game Closure SDK.
+ *
+ * The Game Closure SDK is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public License v. 2.0 as published by Mozilla.
+
+ * The Game Closure SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public License v. 2.0 for more details.
+
+ * You should have received a copy of the Mozilla Public License v. 2.0
+ * along with the Game Closure SDK.  If not, see <http://mozilla.org/MPL/2.0/>.
+ */
+
+// Internationalization plugin
+
 var fs = require('fs');
 var path = require('path');
 var etag = require('../../etag');
