a024c6184a9c6c99e15122f1bed052f09e1561f9
Mario Balibrera <mario@gameclosure.com>
2013-07-24 15:33:41 -0700

    only serve if at least one translation json exists

--- src/serve/plugins/i18n/plugin.js
+++ src/serve/plugins/i18n/plugin.js
@@ -31,21 +31,24 @@ function serveProject (project) {
 	var langPath = path.join(project.paths.root, 'resources', 'lang');
 
 	if (_trans[id] || !fs.existsSync(langPath)) { return false; }
-	
+
 	var trans = _trans[id] = {};
 	var langs = fs.readdirSync(langPath);
+	var shouldServe = false;
 
 	for (var i = 0; i < langs.length; i++) {
 		var fname = langs[i];
 		var lparts = fname.split('.');
 		if (lparts[0] != 'all' && lparts[1] == 'json') {
-			var lang = lparts[0];
-			trans[lang] = JSON.parse(fs.readFileSync(path.join(langPath, fname)));
+			shouldServe = true;
+			trans[lparts[0]] = JSON.parse(fs.readFileSync(path.join(langPath, fname)));
 		}
 	}
 
-	fs.writeFileSync(path.join(langPath, 'all.json'), JSON.stringify(trans));
-	_app.use('/simulate/' + id + '/lang/', etag.static(langPath));
+	if (shouldServe) {
+		fs.writeFileSync(path.join(langPath, 'all.json'), JSON.stringify(trans));
+		_app.use('/simulate/' + id + '/lang/', etag.static(langPath));
+	}
 }
 
 exports.load = function (app) {
