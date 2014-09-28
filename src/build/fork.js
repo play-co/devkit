db15e1189afbc5f407b5f3e5cf4a02f4827813f1
Martin Hunt <martin@gameclosure.com>
2014-09-28 08:16:20 -0700

    forked node process must call process.exit() to die when build finishes

--- src/build/fork.js
+++ src/build/fork.js
@@ -5,4 +5,5 @@ var build = require('./index');
 var args = JSON.parse(process.argv[2]);
 build.build(args.appPath, args.buildOpts, function (err, res) {
   process.send({err: err, res: res});
+  process.exit();
 });
