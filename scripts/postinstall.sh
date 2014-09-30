741bfa2078c35fdeb73b2a2b9984e8a553f8a197
Martin Hunt <martin@gameclosure.com>
2014-09-29 17:39:20 -0700

    create cache directory and config.json in postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,5 +7,13 @@ git submodule update --init
 # reset .gitmodules
 git checkout .gitmodules
 
+# create a cache directory
+mkdir -p cache
+
+# create an empty config.json file
+if [ ! -e config.json ]; then
+	echo "{}" > config.json
+fi
+
 # ensure java is installed
 command -v java >/dev/null 2>&1 || { echo >&2 "java is required. please install java and try again."; exit 1; }
