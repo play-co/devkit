3df5984d5931ce50919180f4b9b3e5a4e44d11c9
Joe Wilm <joe@gameclosure.com>
2015-02-08 19:02:36 -0800

    Update formatting in template app

--- src/apps/templates/default/src/Application.js
+++ src/apps/templates/default/src/Application.js
@@ -3,16 +3,21 @@ import ui.TextView as TextView;
 exports = Class(GC.Application, function () {
 
   this.initUI = function () {
-    var textview = new TextView({
+
+    this.tvHelloWorld = new TextView({
       superview: this.view,
-      text: "Hello, world!",
-      color: "white",
+      text: 'Hello, world!',
+      color: 'white',
       x: 0,
       y: 100,
       width: this.view.style.width,
       height: 100
     });
+
   };
 
-  this.launchUI = function () {};
+  this.launchUI = function () {
+
+  };
+
 });
