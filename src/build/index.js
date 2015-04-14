1783d36bb6471a7227193a87e412bb2a0d4782a6
Martin Hunt <martin@gameclosure.com>
2015-04-13 17:31:31 -0700

    remove build-info json

--- src/build/index.js
+++ src/build/index.js
@@ -1,8 +1,5 @@
 var ff = require('ff');
-var fs = require('fs');
-var path = require('path');
 var chalk = require('chalk');
-var printf = require('printf');
 var logger = require('../util/logging').get('build');
 var apps = require('../apps');
 
@@ -17,72 +14,9 @@ exports.build = function (appPath, argv, cb) {
       elapsed: elapsed,
       config: config
     }, res));
-
-    if (!argv['get-config']) {
-      setBuildInfo('active', false);
-      setBuildInfo('timeStopped', Date.now());
-    }
   }
 
   var onlyGetConfig = argv['get-config'];
-
-  // TODO: Would be nice to use a lib for mirroring this object on dist instead
-  // of these custom functions
-  var buildInfoPath;
-  /** key is optional. if no key, return the whole object */
-  var getBuildInfo = function(key) {
-    if (!buildInfoPath) {
-      logger.error('Cannot GET build info before buildInfoPath is set');
-      return null;
-    }
-
-    var buildInfo;
-    try {
-      buildInfo = JSON.parse(fs.readFileSync(buildInfoPath));
-    } catch(err) {
-      logger.error('Error while getting build info: ' + err.message);
-      buildInfo = {};
-    }
-
-    if (key) {
-      return buildInfo[key];
-    }
-    return buildInfo;
-  }
-  /** if no value is set, key is assumed to be whole object and old object obliterated */
-  var setBuildInfo = function(key, value) {
-    if (onlyGetConfig) { return; }
-
-    if (!buildInfoPath) {
-      logger.error('Cannot SET build info before buildInfoPath is set');
-      return;
-    }
-    try {
-      if (value === undefined) {
-        if (typeof key == 'object') {
-          fs.writeFileSync(buildInfoPath, JSON.stringify(key));
-        } else {
-          logger.error('Cannot write non object for buildInfo' + key);
-          return;
-        }
-      } else {
-        var buildInfo = getBuildInfo();
-        buildInfo[key] = value;
-        fs.writeFileSync(buildInfoPath, JSON.stringify(buildInfo));
-      }
-    } catch(err) {
-      logger.error('Error while setting buildInfo: ' + err.message);
-      buildInfoPath = null;
-    }
-  }
-  /** Increments currentStep in buildInfo, and then continues f chain */
-  var incrementBuildInfoStep = function() {
-    var currentStep = getBuildInfo('currentStep');
-    if (currentStep !== undefined && currentStep !== null) {
-      setBuildInfo('currentStep', currentStep + 1);
-    }
-    f.pass.apply(f, arguments);
-  }
 
   var app;
   var _hasLock = false;
@@ -120,46 +54,32 @@ exports.build = function (appPath, argv, cb) {
     config = res;
     require('./steps/createDirectories').createDirectories(app, config, f());
   }, function () {
-    // Set up the .buildInfo file
-    // TODO: if there is an error before this point, we fail silently.  This is bad
-    buildInfoPath = config.outputPath + '/.buildInfo';
-
-    // Overwrite old one
-    setBuildInfo({
-      timeStarted: startTime,
-      timeStopped: 0,
-      active: true,
-      currentStep: 0,
-      steps: 8, // Number of times incrementBuildInfoStep will be called
-      errors: ''
-    });
-  }, incrementBuildInfoStep, function () {
     require('./steps/buildHooks').getDependencies(app, config, f());
-  }, incrementBuildInfoStep, function (deps) {
+  }, function (deps) {
     // deps is an array of objects, merge them into one object and get all keys with false values
     deps = merge.apply(this, deps);
     var toRemove = Object.keys(deps).filter(function (name) { return deps[name] === false; });
     app.removeModules(toRemove);
-  }, incrementBuildInfoStep, function () {
+  }, function () {
     require('./steps/addDebugCode')(app, config, f());
-  }, incrementBuildInfoStep, function () {
+  }, function () {
     require('./steps/moduleConfig').getConfig(app, config, f());
-  }, incrementBuildInfoStep, function () {
+  }, function () {
     require('./steps/buildHooks').onBeforeBuild(app, config, f());
-  }, incrementBuildInfoStep, function () {
+  }, function () {
     // Skip to success
     if (onlyGetConfig) {
       // ONLY print config to stdout
       console.log(JSON.stringify(merge({title: app.manifest.title}, config)));
-      f.succeed();
+      process.exit(0);
     } else {
       require('./steps/logConfig').log(app, config, f());
     }
-  }, incrementBuildInfoStep, function () {
+  }, function () {
     require('./steps/executeTargetBuild').build(app, config, f());
-  }, incrementBuildInfoStep, function () {
+  }, function () {
     require('./steps/buildHooks').onAfterBuild(app, config, f());
-  }, incrementBuildInfoStep)
+  })
     .error(function (err) {
       if (err.code == 'EEXIST' && !_hasLock) {
         return logger.error('another build is already in progress');
@@ -173,8 +93,6 @@ exports.build = function (appPath, argv, cb) {
         errMsg = err;
       }
       logger.error(errMsg);
-
-      setBuildInfo('errors', errMsg);
     })
     .success(function () {
       logger.log("build succeeded");
@@ -194,8 +112,8 @@ exports.build = function (appPath, argv, cb) {
       logger.log((minutes ? minutes + ' minutes, ' : '') + seconds + ' seconds elapsed');
     })
     .cb(onFinish);
-}
+};
 
 exports.showHelp = function (app, /* optional */ target) {
   require('./steps/executeTargetBuild').showHelp(app, target);
-}
+};
