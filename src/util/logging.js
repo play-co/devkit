a74446ab52fa84182bf9e2d1f306fc548b1cde27
Martin Hunt <martin@gameclosure.com>
2015-04-13 17:35:10 -0700

    linting

--- src/util/logging.js
+++ src/util/logging.js
@@ -9,10 +9,10 @@ var errorToString = require('./toString').errorToString;
  * note: always print to stderr. stdout is for passing stuff between our processes.
  */
 
-var _loggers = {};
+
 exports.get = function (name, isSilent, buffers) {
   return new exports.Logger(name, isSilent, buffers);
-}
+};
 
 exports.Logger = Class(Writable, function () {
   this._outHadNewLine = true;
@@ -36,14 +36,14 @@ exports.Logger = Class(Writable, function () {
 
     this.out = bind(this, function (data) { this._buffer('out', data); });
     this.err = bind(this, function (data) { this._buffer('err', data); });
-  }
+  };
 
   this.constructor.indent = function (indent, str) {
     var prefix = new Array(indent + 1).join(' ');
     return str.split('\n').map(function (line) {
       return prefix + line;
     }).join('\n');
-  }
+  };
 
   this.format = function (str) {
     if (str instanceof Error) {
@@ -54,7 +54,7 @@ exports.Logger = Class(Writable, function () {
       return str;
     }
 
-    return ('' + str)
+    return ('' + str);
 
       // add colour to our build logs so that it's easier to see if and where things went wrong.
       //.replace(/\d*(^|\s|[^a-zA-Z0-9-])error(s|\(s\))?/gi, function (res) { return chalk.redBright(res); })
@@ -62,26 +62,26 @@ exports.Logger = Class(Writable, function () {
 
       // fix new lines
       //.replace(/\r?\n(?!$)/g, '\n' + this._prefix);
-  }
+  };
 
   // call these for formatted logging from code
   this.debug =
   this.info =
   this.log = function () {
     console.error.apply(console, [this._prefix].concat(Array.prototype.map.call(arguments, this.format, this)));
-  }
+  };
 
   this.warn = function () {
     console.error.apply(console, [this._prefix, chalk.red('[warn] ')].concat(Array.prototype.map.call(arguments, this.format, this)));
-  }
+  };
 
   this.error = function () {
     console.error.apply(console, [this._prefix, chalk.red('[error] ')].concat(Array.prototype.map.call(arguments, this.format, this)));
-  }
+  };
 
   this._write = function (chunk, encoding, cb) {
     this._buffer('log', chunk, encoding, cb);
-  }
+  };
 
   this._buffer = function (buffer, chunk, encoding, cb) {
     var data = String(chunk);
@@ -99,17 +99,17 @@ exports.Logger = Class(Writable, function () {
     }
 
     cb && cb();
-  }
+  };
 
   this.toString = function() {
     return this._buffer.join("\n");
-  }
+  };
 
-  this.setLevel = function () {}
-  this.getPrefix = function () { return this._prefix; }
+  this.setLevel = function () {};
+  this.getPrefix = function () { return this._prefix; };
 });
 
 exports.getPrefix = function (tag) {
   return chalk.cyan(printf('%15s   ', '[' + tag + ']'));
-}
+};
 
